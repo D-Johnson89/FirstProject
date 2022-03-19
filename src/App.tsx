9ea0ba99567a767d7879e6a1d6cfdaa159430947
Dustin Johnson <d.l.johnson3009@gmail.com>
2022-03-19 14:42:59 -0400

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Here is something I just wrote <code>Dustin App</code>.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
+}
+
+export default App;
